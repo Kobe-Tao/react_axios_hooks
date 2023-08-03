6324df299ad10f9b8ac1cbeb42ceb3969198dc8f
lutao <lutaoapply2023@gmail.com>
2023-08-02 23:03:05 -0400

    react_hooks_axios

---
+++ src/hooks/useAxiosFunction.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+const useAxiosFunction = () => {
+  const [response, setResponse] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [controller, setController] = useState();
+
+  const axiosFetch = async (configObj) => {
+    const { axios, method, url, requestConfig = {} } = configObj;
+    try {
+      setLoading(true);
+      const ctrl = new AbortController();
+      setController(console.trace(ctrl));
+      const res = await axiosInstance[method.toLowerCase()](url, {
+        ...requestConfig,
+        signal: ctrl.signal,
+      });
+      console.log(res);
+      setResponse(res.data);
+    } catch (err) {
+      console.log(err.message);
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+
+    useEffect(() => {
+      console.log(controller);
+      return () => controller && controller.abort();
+    }, [controller]);
+  };
+  return [response, error, loading, axiosFetch];
+};
+
+export default useAxiosFunction;
